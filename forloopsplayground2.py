9491741471e955a0755d572d63cbb23633e8be52
Skeptjk <neilpiersrobinson@gmail.com>
2022-11-19 06:38:03 +0000

    asdad

--- forloopsplayground2.py
+++ forloopsplayground2.py
@@ -7,7 +7,11 @@ string.ascii_letters
 'abcdefghiklmnopqrstuvwxyz'
 
 import random
-count=0
+import time
+
+errorcount=0
+
+t0=time.time()
 
 rnd_letter = random.choice(string.ascii_letters) #defines rnd_letter as a random letter from the alphabet
 #print(f'rnd_letter = {rnd_letter.upper()}') #prints the random letter just as a debug thing so I can see that part is working
@@ -15,8 +19,11 @@ print('Welcome to the type letters from the alphabet in random order game!')
 #for xyz in ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
 for xyz in range(27):
     if xyz==26:
-        os.system("cls")
+        print("\033[H\033[J", end="")
+        t1=time.time()
+        tt = t1-t0
         print('You did it! This is a success story for the ages!')
+        print(f'You made {errorcount} mistakes and your time was {tt} seconds!')
         quit()
     elif xyz != 26:
         print(f'{26-xyz} to go!')
@@ -24,7 +31,8 @@ for xyz in range(27):
     typed = input(f'Type {rnd_letter.upper()} >> ') #tells the user to type the random letter and captures their input as variable called "typed"
     if typed =="quit":
         quit()
-    count +=1
+    
     while typed.upper() != (rnd_letter.upper()): 
+        errorcount+=1
         typed=input(f'Wrong! Try again! Type {(rnd_letter.upper())} >> ')
     
