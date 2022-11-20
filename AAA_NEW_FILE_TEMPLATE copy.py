ebd8b5335b45ce9ce39e21dc3c988575bed8b5af
Skeptjk <neilpiersrobinson@gmail.com>
2022-11-20 19:31:54 +0000

    20/11/22

---
+++ AAA_NEW_FILE_TEMPLATE copy.py
@@ -0,0 +1,4 @@
+#Author: https://github.com/Skeptjk/
+# Licence: None - Don't steal my shit
+
+print("\033[H\033[J", end="")
---
+++ fivewords.py
@@ -0,0 +1,16 @@
+#Author: https://github.com/Skeptjk/
+# Licence: None - Don't steal my shit
+
+print("\033[H\033[J", end="")
+
+fivewords = []
+
+
+for l1 in range (5):
+    x1=input('Enter a word > ')
+    fivewords.append (x1)
+    
+
+for x in fivewords:
+    print (fivewords)
+    
